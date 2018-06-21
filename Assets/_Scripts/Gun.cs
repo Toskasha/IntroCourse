0c235cfe52ff4e637c7c799e698d5d545306a9f7
Holiday Course <holiday@ad.aie.edu>
2018-06-20 20:59:46 -0700

    Review, added gun script, spawned bullets

---
+++ Assets/_Scripts/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public int Mag = 20;
+
+    public float FireRate = .1f;
+
+    private float SetFireDelay;
+
+    public GameObject bulletPrefab;
+
+    public void Fire()
+    {
+         if(Mag > 0 && SetFireDelay <= 0)
+        {
+            GameObject TempBullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            TempBullet.GetComponent<Rigidbody>().velocity = transform.forward * 100;
+
+            SetFireDelay = FireRate;
+            Mag--;
+        }
+
+    }
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetFireDelay -= Time.deltaTime;
+	}
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody RB;
+
+    public float WalkAcclerationSpeed; // How fast we speed up
+    public float SprintAcclerationSpeed; // How fast we speed up
+
+    private float SetAccelerationSpeed;
+
+    public float MaxWalkSpeed; // Max speed while walking
+    public float MaxSprintSpeed; // max speed while sprinting
+
+    private float SetMaxSpeed; //actual private variable that we use to clamp the speed
+
+    public float RotateSpeed;
+
+    public Animator Anim; // Animator
+
+    private Vector3 IP; //Input vector
+
+    private Camera cam;
+
+    public Gun gun;
+
+	// Use this for initialization
+	void Start ()
+    {
+        RB = GetComponent<Rigidbody>();
+
+        
+
+        cam = GameObject.FindObjectOfType<Camera>();
+        if(cam == null)
+        {
+            Debug.Log("NO CAMERA");
+        }
+	}
+
+    public void updateAnim()
+    {
+        Vector3 localVel = transform.InverseTransformDirection(RB.velocity);
+
+        Anim.SetFloat("ForwardSpeed", localVel.z);
+        Anim.SetFloat("RightSpeed", localVel.x);
+    }
+
+    public void keyInput()
+    {
+        if (Input.GetMouseButton(0)) gun.Fire();
+        IP.x = Input.GetAxisRaw("Horizontal");
+        IP.z = Input.GetAxisRaw("Vertical");
+
+        SetMaxSpeed = (Input.GetButton("Sprint")) ? MaxSprintSpeed : MaxWalkSpeed;
+        SetAccelerationSpeed = (Input.GetButton("Sprint")) ? SprintAcclerationSpeed : WalkAcclerationSpeed;
+    }
+
+    public void handleMovement()
+    {
+      
+        RB.AddForce(IP * Time.deltaTime * SetAccelerationSpeed);
+        
+        RB.velocity = new Vector3(Mathf.Clamp(RB.velocity.x, -SetMaxSpeed, SetMaxSpeed),
+                                  RB.velocity.y,
+                                  Mathf.Clamp(RB.velocity.z, -SetMaxSpeed, SetMaxSpeed));
+    }
+
+    public void doMouseLook()
+    {
+        RaycastHit hit;
+
+        Ray ray = cam.ScreenPointToRay
+            (Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 10000))
+        {
+            Vector3 forward = (transform.position - hit.point).normalized * -1;
+
+            transform.forward = Vector3.MoveTowards
+                (transform.forward, forward, RotateSpeed * Time.deltaTime);
+
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        keyInput();
+        handleMovement();
+        updateAnim();
+        doMouseLook();
+	}
+
+
+}
