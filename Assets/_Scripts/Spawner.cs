2ea2061a60de575f105424d628c86d54182591da
Holiday Course <holiday@ad.aie.edu>
2018-08-01 20:25:52 -0700

    Added textures, lioghting, and enemy spawner

---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject Target;
+    public GameObject AIPrefab;
+    public float SpawnTime = 5;
+    private float CurrentTime;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentTime -= Time.deltaTime;
+
+        if (CurrentTime <= 0)
+        {
+            GameObject TempAI = Instantiate(AIPrefab, transform.position, transform.rotation);
+            TempAI.GetComponent<AIMonsterController>().Target = Target;
+            CurrentTime = SpawnTime;
+        }
+	}
+}
